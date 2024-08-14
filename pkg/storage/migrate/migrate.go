45d8c39bf8fb6f293b781d47864bf439d4f7932a
Shanni Hsu <broken0827heart@gmail.com>
2024-08-14 17:40:21 +0800

    Simplify information of DB connection for migrate

--- pkg/storage/migrate/migrate.go
+++ pkg/storage/migrate/migrate.go
@@ -3,10 +3,9 @@ package migrate
 import (
 	"fmt"
 	"github.com/go-gormigrate/gormigrate/v2"
-	"github.com/spf13/viper"
-	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"member/pkg/storage"
 )
 
 type Organization struct {
@@ -23,24 +22,8 @@ type User struct {
 }
 
 func Init() {
-	//test
-	userName := viper.GetString("database.mysql.userName")
-	password := viper.GetString("database.mysql.password")
-	netWork := viper.GetString("database.mysql.netWork")
-	host := viper.GetString("database.mysql.host")
-	port := viper.GetInt("database.mysql.port")
-	database := viper.GetString("database.mysql.database")
-
-	//組合sql連線字串
-	addr := fmt.Sprintf("%s:%s@%s(%s:%d)/%s?charset=utf8&parseTime=True", userName, password, netWork, host, port, database)
-	//連接MySQL
-	conn, err := gorm.Open(mysql.Open(addr), &gorm.Config{})
-	if err != nil {
-		fmt.Println("connection to mysql failed:", err)
-		return
-	}
-
-	m := gormigrate.New(conn, gormigrate.DefaultOptions, []*gormigrate.Migration{
+	db := storage.InitStorage.GetDBConnect()
+	m := gormigrate.New(db, gormigrate.DefaultOptions, []*gormigrate.Migration{
 		{
 			ID: "202408131743",
 			Migrate: func(tx *gorm.DB) error {
@@ -74,7 +57,7 @@ func Init() {
 		// all other constraints, indexes, etc...
 		return nil
 	})
-	if err = m.Migrate(); err != nil {
+	if err := m.Migrate(); err != nil {
 		log.Fatalf("Migration failed: %v", err)
 	}
 	fmt.Println("Migrate successfully!")
--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"gorm.io/gorm"
+	"log"
 	"member/pkg/storage/mysql"
 )
 
@@ -13,4 +14,13 @@ var InitStorage Storage
 
 func Init() {
 	InitStorage.db = mysql.NewStorage()
+}
+
+// 表示都是使用指標(*Storage)
+// 透過傳遞指標來操控同一個struct的實例
+func (storage *Storage) GetDBConnect() *gorm.DB {
+	if storage.db == nil {
+		log.Fatal("Gat DB connect failed!")
+	}
+	return storage.db
 }
