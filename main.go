f69173bab3619a35fa56ce5f131f27f8a23bdd3d
Shanni Hsu <broken0827heart@gmail.com>
2024-08-21 18:02:49 +0800

    Add Gin Web Api

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"member/config"
 	"member/pkg/storage"
 	"member/pkg/storage/migrate"
+	"member/router"
 )
 
 func main() {
 	config.Init()
 	storage.Init()
 	migrate.Init()
+	router.Init()
 }
