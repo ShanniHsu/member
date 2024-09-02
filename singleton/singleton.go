f4b4f820aa29b94852cff6600a0411638878986d
Shanni Hsu <broken0827heart@gmail.com>
2024-09-02 17:56:53 +0800

    Add comment to Singleton

--- singleton/singleton.go
+++ singleton/singleton.go
@@ -7,6 +7,11 @@ import "sync"
 
 // singleton 是單例模式接口，導出的
 // 通過接口可以避免 GetInstance返回一個包私有類型的指針
+
+// https://huashen87.medium.com/%E5%96%AE%E4%BE%8B%E6%A8%A1%E5%BC%8F-singleton-pattern-5e09dc6d11e7
+// https://geektutu.com/post/hpg-sync-once.html
+// sync.Once是Go標準庫提供的使函數只執行一次的實現，常應用於單例模式，例如初始化配置、保持數據庫連接等。 shanni
+// sync.Once作用與Init函數類似，但有區別，請參考連結二 shanni
 type Singleton interface {
 	foo()
 }
--- singleton/singleton_test.go
+++ singleton/singleton_test.go
@@ -15,9 +15,13 @@ func TestSingleton(t *testing.T) {
 	}
 }
 
+// https://blog.kennycoder.io/2020/12/18/Golang%E6%95%99%E5%AD%B8%E7%B3%BB%E5%88%97-%E4%BD%95%E8%AC%82WaitGroup-%E7%AD%89%E5%BE%85Goroutine%E7%9A%84%E5%A5%BD%E5%B9%AB%E6%89%8B/
+// https://blog.csdn.net/lixora/article/details/129734624
+// goRoutine相關資料 shanni
 func TestParallelSingleton(t *testing.T) {
 	start := make(chan struct{})
 	wg := sync.WaitGroup{}
+	// 代表等待Goroutine的數量 shanni
 	wg.Add(parCount)
 	instances := [parCount]Singleton{}
 	for i := 0; i < parCount; i++ {
@@ -28,7 +32,7 @@ func TestParallelSingleton(t *testing.T) {
 			wg.Done()
 		}(i)
 	}
-	// 關閉channel，所有攜程同時開始進行，實現并行 (parallel)
+	// 關閉channel，所有協程同時開始運行，實現并行 (parallel)
 	close(start)
 	wg.Wait()
 	for i := 1; i < parCount; i++ {
