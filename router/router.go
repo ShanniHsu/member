3f5ba23fa73671b96603f45ce623a8aa46911fdd
Shanni Hsu <broken0827heart@gmail.com>
2024-10-07 17:07:38 +0800

    Adjust router

--- router/router.go
+++ router/router.go
@@ -8,21 +8,25 @@ import (
 )
 
 func Init() {
-	//r := gin.Default()
-	r := gin.New()
+	r := newRouter()
+	r.Run(":8888") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func newRouter() *gin.Engine {
+	router := gin.New()
 	// 註冊上面自定義的日誌中間件
-	r.Use(middleware.Logger())
-	r.GET("/test", func(c *gin.Context) {
+	router.Use(middleware.Logger())
+	router.GET("/test", func(c *gin.Context) {
 		// 查詢我們之前在日誌中間件，注入的鍵值數
 		example := c.MustGet("example").(string)
 		log.Println(example)
 	})
 
-	r.GET("/ping", func(c *gin.Context) {
+	router.GET("/ping", func(c *gin.Context) {
 		panic("errrrrrr")
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run(":8888") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	return router
 }
