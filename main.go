9f0710ee4a9e785476153793b2f578772140b105
Shanni Hsu <broken0827heart@gmail.com>
2024-10-25 17:30:20 +0800

    Add client of Redis

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"member/config"
 	"member/pkg/storage"
 	"member/pkg/storage/migrate"
+	"member/pkg/storage/redis"
 	"member/router"
 )
 
 func main() {
+	redis.ExampleClient()
 	config.Init()
 	storage.Init()
 	migrate.Init()
