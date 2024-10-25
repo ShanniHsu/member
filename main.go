3856f73d3440a6cb55af694fe8d633e69c0cbc64
Shanni Hsu <broken0827heart@gmail.com>
2024-10-25 16:19:10 +0800

    Use argon2 to test password

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"member/config"
+	"member/pkg/argon2"
 	"member/pkg/storage"
 	"member/pkg/storage/migrate"
 	"member/router"
 )
 
 func main() {
+	argon2.TestPrint() //test the argon2
 	config.Init()
 	storage.Init()
 	migrate.Init()
