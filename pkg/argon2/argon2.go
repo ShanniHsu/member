6cf4b3b590d3b4be094b68679e26ef854a03abd4
Shanni Hsu <broken0827heart@gmail.com>
2024-10-25 16:59:28 +0800

    Adjust the return slice value of generateFromPassowrd to be string

--- pkg/argon2/argon2.go
+++ pkg/argon2/argon2.go
@@ -2,6 +2,7 @@ package argon2
 
 import (
 	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"golang.org/x/crypto/argon2"
 	"log"
@@ -35,19 +36,22 @@ func TestPrint() {
 	fmt.Println("hash: ", hash)
 }
 
-func generateFromPassword(password string, p *params) (hash []byte, err error) {
-	// Generate a cryptographically secure random salt.
+func generateFromPassword(password string, p *params) (encodedHash string, err error) {
 	salt, err := generateRandomBytes(p.saltLength)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
-	// Pass the plaintext password, salt and parameters to the argon2.IDKey
-	// function. This will generate a hash of the password using the Argon2id
-	// variant.
-	hash = argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, p.keyLength)
+	hash := argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, p.keyLength)
 
-	return hash, nil
+	// Base64 encode the salt and hashed password.
+	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
+	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
+
+	// Return a string using the standard encoded hash representation.
+	encodedHash = fmt.Sprintf("$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s", argon2.Version, p.memory, p.iterations, p.parallelism, b64Salt, b64Hash)
+
+	return encodedHash, nil
 }
 
 func generateRandomBytes(n uint32) ([]byte, error) {
