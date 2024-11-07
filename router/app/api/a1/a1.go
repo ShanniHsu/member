303e814babc15f74f942d5c13e455321001eea1b
Shanni Hsu <broken0827heart@gmail.com>
2024-11-07 17:08:26 +0800

    Add api group that has authorization to operate

--- router/app/api/a1/a1.go
+++ router/app/api/a1/a1.go
@@ -2,9 +2,11 @@ package a1
 
 import (
 	"github.com/gin-gonic/gin"
+	"member/pkg/jwt"
 	"member/router/app/controller"
 	"member/router/repository"
 	"member/router/service"
+	"net/http"
 )
 
 func Init(router *gin.Engine) {
@@ -13,4 +15,21 @@ func Init(router *gin.Engine) {
 	api := controller.NewAppController(newUserService)
 	router.POST("/register", api.Register)
 	router.POST("/login", api.Login)
+
+	user := router.Group("/user")
+	user.Use(jwt.JWTAuth())
+	user.GET("/info", func(ctx *gin.Context) {
+		claim, err := jwt.GetUserInfo(ctx)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"message": err,
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": claim,
+		})
+		return
+	})
 }
