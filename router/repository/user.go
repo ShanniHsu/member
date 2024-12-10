265a43d80af3af49a415ad7a772290741a589f40
Shanni Hsu <broken0827heart@gmail.com>
2024-12-10 17:49:24 +0800

    Add operation of Redis to UserRepository

--- router/repository/user.go
+++ router/repository/user.go
@@ -1,25 +1,33 @@
 package repository
 
 import (
+	"context"
+	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 	"member/models"
+	"time"
 )
 
 // method
 type UserRepository interface {
 	GetUserByAccount(account string) (resp *models.User, err error)
 	Create(user *models.User) (err error)
+	SetRedis(key string, value string, expiration time.Duration) (err error)
+	GetRedis(key string) (value string, err error)
+	DeleteRedis(key string) (err error)
 }
 
 // the request that is applied the method
 type userRepository struct {
-	DB *gorm.DB
+	DB    *gorm.DB
+	Redis *redis.Client
 }
 
 // use userRepository to return UserRepository(interface)
-func NewUserRepository(db *gorm.DB) UserRepository {
+func NewUserRepository(db *gorm.DB, redis *redis.Client) UserRepository {
 	return userRepository{
-		DB: db,
+		DB:    db,
+		Redis: redis,
 	}
 }
 
@@ -31,4 +39,16 @@ func (r userRepository) GetUserByAccount(account string) (resp *models.User, err
 
 func (r userRepository) Create(user *models.User) (err error) {
 	return r.DB.Create(&user).Error
+}
+
+func (r userRepository) SetRedis(key string, value string, expiration time.Duration) (err error) {
+	return r.Redis.Set(context.Background(), key, value, expiration).Err()
+}
+
+func (r userRepository) GetRedis(key string) (value string, err error) {
+	return r.Redis.Get(context.Background(), key).Result()
+}
+
+func (r userRepository) DeleteRedis(key string) (err error) {
+	return r.Redis.Del(context.Background(), key).Err()
 }
