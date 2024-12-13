a1d6544390d7334b42685f01dccd0c8939cf6d4c
Shanni Hsu <broken0827heart@gmail.com>
2024-12-13 18:01:12 +0800

    Delete function redis.ExampleClient not in need

--- main.go
+++ main.go
@@ -5,12 +5,10 @@ import (
 	"member/pkg/jwt"
 	"member/pkg/storage"
 	"member/pkg/storage/migrate"
-	"member/pkg/storage/redis"
 	"member/router"
 )
 
 func main() {
-	redis.ExampleClient()
 	config.Init()
 	storage.Init()
 	migrate.Init()
--- pkg/storage/redis/redis.go
+++ pkg/storage/redis/redis.go
@@ -2,7 +2,6 @@ package redis
 
 import (
 	"context"
-	"fmt"
 	"github.com/redis/go-redis/v9"
 	"github.com/spf13/viper"
 )
@@ -21,29 +20,4 @@ func NewStorage() (rdb *redis.Client) {
 		DB:       dbName,   // use default DB
 	})
 	return rdb
-}
-
-func ExampleClient() {
-	rdb := NewStorage()
-	err := rdb.Set(ctx, "key", "value", 0).Err()
-	if err != nil {
-		panic(err)
-	}
-
-	val, err := rdb.Get(ctx, "key").Result()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("key", val)
-
-	val2, err := rdb.Get(ctx, "key2").Result()
-	if err == redis.Nil {
-		fmt.Println("key2 does not exist")
-	} else if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("key2", val2)
-	}
-	// Output: key value
-	// key2 does not exist
 }
