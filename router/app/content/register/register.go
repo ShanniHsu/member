a5249dee51b6ca091ef626ddd7e75d1f0ed219dd
Shanni Hsu <broken0827heart@gmail.com>
2025-01-06 17:12:48 +0800

    Add request paramter 'email' to api of register

--- router/app/content/register/register.go
+++ router/app/content/register/register.go
@@ -4,4 +4,5 @@ type Request struct {
 	Account  string `json:"account" binding:"max=10,min=6"`
 	Password string `json:"password" binding:"max=10,min=6"`
 	Nickname string `json:"nickname" binding:"required"`
+	Email    string `json:"email" binding:"email"`
 }
