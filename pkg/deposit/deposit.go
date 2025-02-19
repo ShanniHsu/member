d559fc108299f01046d7072657d1f12930ec961d
Shanni Hsu <broken0827heart@gmail.com>
2025-02-19 14:56:45 +0800

    Add mutex lock to test

--- pkg/deposit/deposit.go
+++ pkg/deposit/deposit.go
@@ -2,16 +2,20 @@ package deposit
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
+
+var mu = new(sync.Mutex)
 
 var balance int64 = 1000 // 原始存款餘額
 
 func deposit(amount int64) {
+	mu.Lock() //獲取鎖，確保一個goroutine修改balance
+	defer mu.Unlock()
 	balance += amount
 }
 
-// 因為沒有使用鎖，多個gorutine同時讀取balance，導致寫入時發生競爭條件(race condition)，造成錯誤結果(這邊列印出來是10640而非預期結果11000)
 func UseDeposit() {
 	for i := 0; i < 1000; i++ {
 		go deposit(10)
