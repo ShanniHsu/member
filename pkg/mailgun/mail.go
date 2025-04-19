ea935c39aae8b313791507d9e6c59f38f4a46e63
Shanni Hsu <broken0827heart@gmail.com>
2025-04-19 14:56:51 +0800

    Add send mail function

--- pkg/mailgun/mail.go
+++ pkg/mailgun/mail.go
@@ -1,1 +1,42 @@
 package mailgun
+
+import (
+	"context"
+	"fmt"
+	"github.com/mailgun/mailgun-go/v4"
+	"github.com/spf13/viper"
+	"log"
+	"time"
+)
+
+// Your available domain names can be found here:
+// (https://app.mailgun.com/app/domains)
+var yourDomain string = viper.GetString("mailgun.domain") // e.g. mg.yourcompany.com
+
+// You can find the Private API Key in your Account Menu, under "Settings":
+// (https://app.mailgun.com/app/account/security)
+var privateAPIKey string = viper.GetString("mailgun.privateAPIKey")
+
+var sender string = viper.GetString("mailgun.sender")
+
+func SendMail(subject string, email string, body string) (err error) {
+	// Create an instance of the Mailgun Client
+	mg := mailgun.NewMailgun(yourDomain, privateAPIKey)
+	//When you have an EU-domain, you must specify the endpoint:
+	//mg.SetAPIBase("https://api.eu.mailgun.net/v3")
+
+	recipient := email
+	// The message object allows you to add attachments and Bcc recipients
+	message := mg.NewMessage(sender, subject, body, recipient)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	// Send the message with a 10 second timeout
+	resp, id, err := mg.Send(ctx, message)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Printf("ID: %s Resp: %s\n", id, resp)
+	return
+}
