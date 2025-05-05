ab7e8a2735e463c5a389010076107544855887fd
Shanni Hsu <broken0827heart@gmail.com>
2025-05-05 16:43:08 +0800

    Add redis.Exists (if redis key is exist, it will skip)

--- router/service/seats.go
+++ router/service/seats.go
@@ -11,6 +11,11 @@ func InitSeats() {
 	seats := []string{"A1", "A2", "A3", "A4", "B1", "B2"}
 
 	for _, seat := range seats {
+		exists, _ := rdb.Exists(context.Background(), fmt.Sprintf("seat:%s:status", seat)).Result()
+		if exists != 0 {
+			fmt.Println("Seats already initialized, skip.")
+			continue
+		}
 		// 座位初始都是 available
 		err := rdb.Set(context.Background(), fmt.Sprintf("seat:%s:status", seat), "available", 0).Err()
 		if err != nil {
