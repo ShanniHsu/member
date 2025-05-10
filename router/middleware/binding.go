e80f380824971d3804e41152305a9c8c661aef2d
Shanni Hsu <broken0827heart@gmail.com>
2025-05-11 01:15:04 +0800

    Vaildate add message

--- router/middleware/binding.go
+++ router/middleware/binding.go
@@ -17,10 +17,32 @@ func Binding[T any]() gin.HandlerFunc {
 			return
 		}
 
+		message := "Please check your input"
 		if err := validator.New().Struct(obj); err != nil {
+			// 如果錯誤不是validator回傳的
+			if _, ok := err.(validator.ValidationErrors); !ok {
+				ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+					"error":  "Validation failed",
+					"detail": err.Error(),
+				})
+				return
+			}
+
+			validatorErr := err.(validator.ValidationErrors)[0]
+			switch validatorErr.Tag() {
+			case "required":
+				message = validatorErr.Field() + " is required"
+			case "max":
+				message = validatorErr.Field() + " is too long"
+			case "min":
+				message = validatorErr.Field() + " is too short"
+			case "email":
+				message = validatorErr.Field() + " is invalid format"
+			}
+
 			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-				"error":  "Validation failed",
-				"detail": err.Error(),
+				"error":  message,
+				"detail": validatorErr.Error(),
 			})
 			return
 		}
