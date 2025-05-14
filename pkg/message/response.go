f5d011c678f61f5aba37eb07c4b7f35a0b727c36
Shanni Hsu <broken0827heart@gmail.com>
2025-05-15 01:34:26 +0800

    feat: Unify API response format

---
+++ pkg/message/response.go
@@ -0,0 +1,23 @@
+package message
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// 補充: omitempty是將未賦值的字段省略(0,"")
+// 更深入研究就回頭看 https://cloud.tencent.com/developer/article/2224409
+type Response struct {
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data,omitempty"`
+}
+
+func (r Response) ResponseSuccess(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, r)
+	return
+}
+
+func (r Response) ResponseBadRequest(ctx *gin.Context) {
+	ctx.JSON(http.StatusBadRequest, r)
+	return
+}
