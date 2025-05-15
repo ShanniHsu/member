30e391a6d0a3aa4fe50dd32cfc067cb10ffe5664
Shanni Hsu <broken0827heart@gmail.com>
2025-05-15 17:13:30 +0800

    refactor: auth middleware use ruled response

--- router/app/middleware/auth.go
+++ router/app/middleware/auth.go
@@ -2,26 +2,23 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
-	"member/models"
 	"member/pkg/jwt"
+	"member/pkg/message"
 	"member/router/service"
-	"net/http"
 	"strings"
 )
 
 // 路由請求中間件，前端必須把token放到請求頭上，對服務器進行驗證token成功後，才能訪問後續的請求路由
 func Auth(userService service.User) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		var user = new(models.User)
-
+		resp := message.Response{Msg: "System error"}
 		// 獲取authorization header: 獲取前端傳過來的訊息
 		tokenString := ctx.GetHeader("Authorization")
 
 		// 驗證前端傳過來的token格式，須不為空，且開頭為Bearer
 		if tokenString == "" || !strings.HasPrefix(tokenString, "Bearer ") {
-			ctx.JSON(http.StatusUnauthorized, gin.H{
-				"message": "Unauthorized!",
-			})
+			resp.Msg = "Unauthorized!"
+			resp.ResponseUnauthorized(ctx)
 			ctx.Abort()
 			return
 		}
@@ -30,18 +27,16 @@ func Auth(userService service.User) gin.HandlerFunc {
 		token := tokenString[7:]
 		claims, err := jwt.ParseToken(token)
 		if err != nil {
-			ctx.JSON(http.StatusUnauthorized, gin.H{
-				"message": "Token is invalid or expired!",
-			})
+			resp.Msg = "Token is invalid or expired!"
+			resp.ResponseUnauthorized(ctx)
 			ctx.Abort()
 			return
 		}
 
-		user, err = userService.AuthBearerToken(claims.Token)
+		user, err := userService.AuthBearerToken(claims.Token)
 		if err != nil {
-			ctx.JSON(http.StatusUnauthorized, gin.H{
-				"message": "Token is invalid or expired!",
-			})
+			resp.Msg = "Token is invalid or expired!"
+			resp.ResponseUnauthorized(ctx)
 			ctx.Abort()
 			return
 		}
