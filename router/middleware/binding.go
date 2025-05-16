dbcf337d6d75ce6c858935140d04484c6fd389c5
Shanni Hsu <broken0827heart@gmail.com>
2025-05-16 16:38:46 +0800

    refactor: binding use ruled response

--- router/middleware/binding.go
+++ router/middleware/binding.go
@@ -3,47 +3,48 @@ package middleware
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
-	"net/http"
+	"log"
+	"member/pkg/message"
 )
 
 func Binding[T any]() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		resp := message.Response{Msg: "System error"}
 		var obj T
 		if err := ctx.ShouldBindJSON(&obj); err != nil {
-			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-				"error":  "Invalid request body",
-				"detail": err.Error(),
-			})
+			resp.Msg = "Invalid request body"
+			resp.ResponseBadRequest(ctx)
+			ctx.Abort()
 			return
 		}
 
-		message := "Please check your input"
+		msg := "Please check your input"
 		if err := validator.New().Struct(obj); err != nil {
 			// 如果錯誤不是validator回傳的
 			if _, ok := err.(validator.ValidationErrors); !ok {
-				ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-					"error":  "Validation failed",
-					"detail": err.Error(),
-				})
+				resp.Msg = "Validation failed"
+				resp.ResponseBadRequest(ctx)
+				log.Println("error:", err.Error())
+				ctx.Abort()
 				return
 			}
 
 			validatorErr := err.(validator.ValidationErrors)[0]
 			switch validatorErr.Tag() {
 			case "required":
-				message = validatorErr.Field() + " is required"
+				msg = validatorErr.Field() + " is required"
 			case "max":
-				message = validatorErr.Field() + " is too long"
+				msg = validatorErr.Field() + " is too long"
 			case "min":
-				message = validatorErr.Field() + " is too short"
+				msg = validatorErr.Field() + " is too short"
 			case "email":
-				message = validatorErr.Field() + " is invalid format"
+				msg = validatorErr.Field() + " is invalid format"
 			}
 
-			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-				"error":  message,
-				"detail": validatorErr.Error(),
-			})
+			resp.Msg = msg
+			resp.ResponseBadRequest(ctx)
+			log.Println("error:", validatorErr.Error())
+			ctx.Abort()
 			return
 		}
 
