ae384d40d2dfc21f38b8cab43d61d3bb7f295335
Shanni Hsu <broken0827heart@gmail.com>
2025-05-26 20:25:05 +0800

    fix: redis address miss port

--- pkg/storage/redis/redis.go
+++ pkg/storage/redis/redis.go
@@ -1,12 +1,9 @@
 package redis
 
 import (
-	"context"
 	"github.com/redis/go-redis/v9"
 	"github.com/spf13/viper"
 )
-
-var ctx = context.Background()
 
 func NewStorage() (rdb *redis.Client) {
 	host := viper.GetString("database.redis.host")
@@ -15,7 +12,7 @@ func NewStorage() (rdb *redis.Client) {
 	dbName := viper.GetInt("database.redis.dbName")
 
 	rdb = redis.NewClient(&redis.Options{
-		Addr:     host + port,
+		Addr:     host + ":" + port,
 		Password: password, // no password set
 		DB:       dbName,   // use default DB
 	})
